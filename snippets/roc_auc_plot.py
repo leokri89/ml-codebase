c9bd9c8443837725262d83f200aadfceda07719f
leokri89 <66442632+leokri89@users.noreply.github.com>
2021-09-01 15:05:06 -0300

    Create roc_auc_plot.py

---
+++ snippets/roc_auc_plot.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import roc_curve, roc_auc_score
+
+
+def roc_auc_graph(y_test, y_predict):
+    false_positive_rate, true_positive_rate, _ = roc_curve(y_test, y_predict)
+    AUC = roc_auc_score(y_test, y_predict)
+
+    data = dict(false_positive_rate = false_positive_rate, true_positive_rate = true_positive_rate)
+
+    ROC = pd.DataFrame(data)
+    x = np.linspace(0, 1, len(ROC))
+    y = np.linspace(0, 1, len(ROC))
+
+    plot_title = "AUC: {:.4f}".format(AUC)
+    plt.title(plot_title, loc='center', fontsize=14, fontweight=0, color='black')
+
+    plt.plot('False Positive Rate', 'True Positive Rate', data = ROC, marker = '', markersize = 8, color = 'navy', linewidth = 3)
+    plt.plot(x, y, marker='', markersize=10, linestyle='--', color='black', linewidth=1)
+    
+    plt.ylabel("True positive")
+    plt.xlabel("False Positive")
