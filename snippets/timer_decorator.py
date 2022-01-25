55706bf5dbb9ea85341305e55e5bf87848346350
leokri89 <66442632+leokri89@users.noreply.github.com>
2022-01-25 00:56:26 -0300

    Create timer_decorator.py

---
+++ snippets/timer_decorator.py
@@ -0,0 +1,18 @@
+from time import time, sleep
+
+def timer(func):
+    def func_wrap(*args, **kwargs):
+        tstart = time()
+        result = func(*args, **kwargs)
+        tend = time()
+        print(f'Function {func.__name__!r} executed in {(tend-tstart):.3f}s')
+        return result
+    return func_wrap
+
+@timer
+def looping(n):
+    for i in range(n):
+        sleep(1)
+
+
+looping(10)
